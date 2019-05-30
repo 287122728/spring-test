d29f82039f5b3e63c1019f23fc83c47fb0eab031
lib01 <Apple@192.168.1.76>
2019-05-30 14:13:09 +0800

    mvc

---
+++ mvc/src/main/java/com/crush/test/spring/mvc/SpringMvcTestBootstrap.java
@@ -0,0 +1,29 @@
+package com.crush.test.spring.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * Title: TODO
+ * </p>
+ * <p>
+ * Description: TODO
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2017
+ * </p>
+ * <p>
+ * Company: 客如云
+ * </p>
+ *
+ * @author crush_lee
+ * @date 2019/5/30
+ */
+@SpringBootApplication
+public class SpringMvcTestBootstrap {
+    public static void main(String...argc){
+        SpringApplication.run(SpringMvcTestBootstrap.class);
+    }
+
+}
